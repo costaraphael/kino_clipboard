2a5e6a1a06a6d7a347525c2e732d75f7b8351987
Raphael Costa <vidal.raphael@gmail.com>
2025-01-21 22:21:45 +0000

    Release 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KinoClipboard.MixProject do
   def project do
     [
       app: :kino_clipboard,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
